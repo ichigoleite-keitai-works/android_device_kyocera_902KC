e6b14946ec1b5bffc5146285a6040037bb074491
PicelBoi <picelboi@picelboi.xyz>
2026-06-23 14:15:26 -0400

    change var

--- 902KC.mk
+++ 902KC.mk
@@ -3,7 +3,9 @@ DEVICE_PACKAGE_OVERLAYS += device/kyocera/902KC/overlay
 # Inherit product configuration from dependencies
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-$(call inherit-product, $(DEVICE_PATH)/go_defaults.mk)
+
+# Go time.
+$(call inherit-product, $(LOCAL_PATH)/go_defaults.mk)
 
 
 PRODUCT_AAPT_CONFIG := normal
