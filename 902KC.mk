c5a06c8253bb9389a0b6164dd6ad445d5e3ab4fe
ichigoleite <picelboi@picelboi.xyz>
2026-09-02 21:48:43 -0400

    update 902KC

--- 902KC.mk
+++ 902KC.mk
@@ -1,6 +1,7 @@
+# Must define platform variant before including any common things
+TARGET_BOARD_PLATFORM_VARIANT := msm8909
+
 DEVICE_PACKAGE_OVERLAYS += device/kyocera/902KC/overlay
-# Automatically add overlays
-LOCAL_AAPT_FLAGS += --auto-add-overlay
 
 # Inherit product configuration from dependencies
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -12,9 +13,11 @@ $(call inherit-product, vendor/kyocera/902KC/902KC-vendor.mk)
 # Go time.
 $(call inherit-product, $(LOCAL_PATH)/modules/android_go.mk)
 
+# Properties
+-include $(LOCAL_PATH)/system_prop.mk
 
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := hdpi
+PRODUCT_AAPT_PREF_CONFIG := 240dpi
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
@@ -38,9 +41,31 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     tinymix
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/acdb/Bluetooth_cal.acdb:system/etc/Bluetooth_cal.acdb \
+    $(LOCAL_PATH)/audio/acdb/General_cal.acdb:system/etc/General_cal.acdb \
+    $(LOCAL_PATH)/audio/acdb/Global_cal.acdb:system/etc/Global_cal.acdb \
+    $(LOCAL_PATH)/audio/acdb/Handset_cal.acdb:system/etc/Handset_cal.acdb \
+    $(LOCAL_PATH)/audio/acdb/Hdmi_cal.acdb:system/etc/Hdmi_cal.acdb \
+    $(LOCAL_PATH)/audio/acdb/Headset_cal.acdb:system/etc/Headset_cal.acdb \
+    $(LOCAL_PATH)/audio/acdb/Speaker_cal.acdb:system/etc/Speaker_cal.acdb \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
+
 # Camera
 PRODUCT_PACKAGES += \
-    camera.msm8909
+    camera.msm8909 \
+    libboringssl-compat \
+    libstlport \
+    Snap
 
 # CNE
 PRODUCT_PACKAGES += \
@@ -56,6 +81,8 @@ PRODUCT_PACKAGES += \
     copybit.msm8909 \
     gralloc.msm8909 \
     hwcomposer.msm8909 \
+    libion \
+    libtinyxml \
     memtrack.msm8909
 
 # Ebtables
@@ -73,6 +100,24 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     gps.msm8909
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+
+# IRSC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl \
+    $(LOCAL_PATH)/keylayout/matrix_keypad_67.kl:system/usr/keylayout/matrix_keypad_67.kl \
+    $(LOCAL_PATH)/keylayout/msm8909-pm8916-snd-card_Button_Jack.kl:system/usr/keylayout/msm8909-pm8916-snd-card_Button_Jack.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl
+
 # Keystore
 PRODUCT_PACKAGES += \
     keystore.msm8909
@@ -81,6 +126,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.msm8909
 
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs_8909.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance_8909.xml:system/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media_profiles_8909.xml:system/etc/media_profiles.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
+
 # Misc
 PRODUCT_PACKAGES += \
     libtinyxml \
@@ -88,15 +144,64 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
-    libextmedia_jni \
+    libOmxAacEnc \
+    libOmxAmrEnc \
     libOmxCore \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_soft_flacdec
 
 # Power
 PRODUCT_PACKAGES += \
     power.msm8909
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.kc.sensor.sh \
+    init.kc.usb.sh \
+    init.qcom.bt.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.crashdata.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.mem.sh \
+    init.qcom.modem_links.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.wifi.sh \
+    init.qti.fm.sh \
+    init.qti.ims.sh \
+    init.qti.qseecomd.sh \
+    init.kc.display.rc \
+    init.kc.leds.rc \
+    init.kc.touch.rc \
+    init.qcom.factory.rc \
+    init.qcom.power.rc \
+    init.qcom.rc \
+    init.qcom.ssr.rc \
+    init.qcom.usb.rc \
+    init.recovery.qcom.rc \
+    init.target.rc
+
+# Sensors
+PRODUCT_PACKAGES += \
+    calmodule.cfg \
+    sensors.msm8909 \
+    sensors.native
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf
+
+# Shims
+PRODUCT_PACKAGES += \
+    libshim_cutils
 
 # Wifi
 PRODUCT_PACKAGES += \
@@ -107,20 +212,6 @@ PRODUCT_PACKAGES += \
     libQWiFiSoftApCfg \
     libwcnss_qmi \
     wcnss_service
-
-# Ramdisk
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,device/kyocera/902KC/ramdisk,root)
-
-# Prebuilt
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,device/kyocera/902KC/prebuilt/system,system)
-
-# Media codecs
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
