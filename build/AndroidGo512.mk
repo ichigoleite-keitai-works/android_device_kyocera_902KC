11c9b226450f7e16cd46879d418e0b6842ac66a5
ichigoleite <picelboi@picelboi.xyz>
2026-09-04 09:42:17 -0400

    remove unused files

--- build/AndroidGo512.mk
+++
@@ -1,7 +0,0 @@
-# 512MB specific properties.
-# lmkd can kill more now.
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lmk.medium=700
-# madvise random in ART to reduce page cache thrashing.
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.madvise-random=true
