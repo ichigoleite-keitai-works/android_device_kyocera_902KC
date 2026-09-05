0d4540f87cc6456b2c2bf50c31100f01d6def7bc
ichigoleite <picelboi@picelboi.xyz>
2026-09-04 21:41:54 -0400

    remove 902kc-keyptrd since this is the lineage branch

--- recovery/Android.mk
+++
@@ -1,4 +0,0 @@
-# Recovery-only keypad to virtual mouse/keyboard bridge. The stock matrix
-# keypad remains the hardware source; TWRP consumes the uinput device emitted
-# by 902kc-keyptrd instead of consuming matrix_keypad directly.
-TARGET_RECOVERY_DEVICE_MODULES += 902kc-keyptrd
