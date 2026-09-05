493150f85268236b3b3cf14bbb9bfe17a1a2cb36
ichigoleite <picelboi@picelboi.xyz>
2026-09-04 22:26:59 -0400

    remove init.rc from init.target.rc

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,6 +5,13 @@
 
 LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.bt.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
+include $(BUILD_PREBUILT)
+
 # Recovery init configuration files
 
 include $(CLEAR_VARS)
